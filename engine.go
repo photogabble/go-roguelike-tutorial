43ff6804f4d7c54dea870ee3353bbda984a7b641
Simon <carbontwelve@users.noreply.github.com>
2022-12-14 23:43:27 +0000

    feat: create Engine struct

---
+++ engine.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/BigJk/ramen/concolor"
+	"github.com/BigJk/ramen/console"
+)
+
+type Engine struct {
+	entities *EntityList
+	player   *Entity
+	gameMap  *GameMap
+}
+
+func (e *Engine) Render(con *console.Console) {
+	e.gameMap.Render(con)
+	e.entities.Render(con)
+}
+
+func NewEngine(player *Entity, gameMap *GameMap) *Engine {
+	engine := &Engine{
+		entities: &EntityList{},
+		gameMap:  gameMap,
+		player:   player,
+	}
+
+	engine.entities.Add(player)
+	engine.entities.Add(NewEntity((ScreenW/2)-5, ScreenH/2, '@', concolor.RGB(255, 255, 0)))
+
+	return engine
+}
