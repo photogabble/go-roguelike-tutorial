8b51897e3532bda79fe8515dd57bc35d7a1d08fb
Simon <carbontwelve@users.noreply.github.com>
2022-12-14 23:46:29 +0000

    refactor: implement Action interface

--- actions.go
+++ actions.go
@@ -1,6 +1,16 @@
 package main
 
+import "os"
+
+type Action interface {
+	Perform(engine *Engine, entity *Entity)
+}
+
 type EscapeAction struct {
+}
+
+func (esc *EscapeAction) Perform(engine *Engine, entity *Entity) {
+	os.Exit(0)
 }
 
 type MovementAction struct {
@@ -8,13 +18,28 @@ type MovementAction struct {
 	dy int
 }
 
-func NewMovementAction(dx, dy int) MovementAction {
-	return MovementAction{
+func (move *MovementAction) Perform(engine *Engine, entity *Entity) {
+	dX := entity.X + move.dx
+	dY := entity.Y + move.dy
+
+	if !engine.gameMap.InBounds(dX, dY) {
+		return // Destination out of bounds
+	}
+
+	if !engine.gameMap.GetTile(dX, dY).Walkable {
+		return // Destination is blocked by a tile
+	}
+
+	entity.Move(move.dx, move.dy)
+}
+
+func NewMovementAction(dx, dy int) *MovementAction {
+	return &MovementAction{
 		dx: dx,
 		dy: dy,
 	}
 }
 
-func NewEscapeAction() EscapeAction {
-	return EscapeAction{}
+func NewEscapeAction() *EscapeAction {
+	return &EscapeAction{}
 }
--- input.go
+++ input.go
@@ -5,7 +5,7 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-func EventHandler() interface{} {
+func EventHandler() Action {
 	if inpututil.IsKeyJustPressed(ebiten.KeyW) {
 		return NewMovementAction(0, -1)
 	}
