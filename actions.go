81957d142bb74532f46fb004244a48c94b7d8bcc
Simon <carbontwelve@users.noreply.github.com>
2022-12-16 14:03:21 +0000

    feat: add Melee and Bump actions

--- actions.go
+++ actions.go
@@ -1,6 +1,9 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 type Action interface {
 	Perform(engine *Engine, entity *Entity)
@@ -13,9 +16,14 @@ func (esc *EscapeAction) Perform(engine *Engine, entity *Entity) {
 	os.Exit(0)
 }
 
-type MovementAction struct {
+// ActionWithDirection is a base struct for Actions with destination
+type ActionWithDirection struct {
 	dx int
 	dy int
+}
+
+type MovementAction struct {
+	ActionWithDirection
 }
 
 func (move *MovementAction) Perform(engine *Engine, entity *Entity) {
@@ -30,13 +38,71 @@ func (move *MovementAction) Perform(engine *Engine, entity *Entity) {
 		return // Destination is blocked by a tile
 	}
 
+	entityAtDestination := engine.gameMap.entities.AtLocation(dX, dY)
+
+	if entityAtDestination != nil && entityAtDestination.blocksMovement == true {
+		return // Destination blocked by Entity
+	}
+
 	entity.Move(move.dx, move.dy)
+}
+
+type MeleeAction struct {
+	ActionWithDirection
+}
+
+func (action *MeleeAction) Perform(engine *Engine, entity *Entity) {
+	dX := entity.X + action.dx
+	dY := entity.Y + action.dy
+
+	target := engine.gameMap.entities.AtLocation(dX, dY)
+
+	if target != nil {
+		fmt.Println(fmt.Sprintf("You kick the %s, much to its annoyance!", target.name))
+	}
+}
+
+type BumpAction struct {
+	ActionWithDirection
+}
+
+func (action *BumpAction) Perform(engine *Engine, entity *Entity) {
+	dX := entity.X + action.dx
+	dY := entity.Y + action.dy
+
+	target := engine.gameMap.entities.AtLocation(dX, dY)
+
+	if target == nil {
+		NewMovementAction(action.dx, action.dy).Perform(engine, entity)
+	} else {
+		NewMeleeAction(action.dx, action.dy).Perform(engine, entity)
+	}
+}
+
+func NewMeleeAction(dx, dy int) *MeleeAction {
+	return &MeleeAction{
+		ActionWithDirection{
+			dx: dx,
+			dy: dy,
+		},
+	}
 }
 
 func NewMovementAction(dx, dy int) *MovementAction {
 	return &MovementAction{
-		dx: dx,
-		dy: dy,
+		ActionWithDirection{
+			dx: dx,
+			dy: dy,
+		},
+	}
+}
+
+func NewBumpAction(dx, dy int) *BumpAction {
+	return &BumpAction{
+		ActionWithDirection{
+			dx: dx,
+			dy: dy,
+		},
 	}
 }
 
