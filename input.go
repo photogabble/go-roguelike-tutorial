c028120c5649c4e3088bfbae000202f11198727e
Simon <carbontwelve@users.noreply.github.com>
2022-12-16 14:11:07 +0000

    refactor: use NewBumpAction

--- input.go
+++ input.go
@@ -8,19 +8,19 @@ import (
 // EventHandler listens for ebiten input events and translates them to Action's.
 func EventHandler() Action {
 	if inpututil.IsKeyJustPressed(ebiten.KeyW) {
-		return NewMovementAction(0, -1)
+		return NewBumpAction(0, -1)
 	}
 
 	if inpututil.IsKeyJustPressed(ebiten.KeyS) {
-		return NewMovementAction(0, 1)
+		return NewBumpAction(0, 1)
 	}
 
 	if inpututil.IsKeyJustPressed(ebiten.KeyA) {
-		return NewMovementAction(-1, 0)
+		return NewBumpAction(-1, 0)
 	}
 
 	if inpututil.IsKeyJustPressed(ebiten.KeyD) {
-		return NewMovementAction(1, 0)
+		return NewBumpAction(1, 0)
 	}
 
 	if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
